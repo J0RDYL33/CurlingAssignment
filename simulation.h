a9e8086c69ddf03a1b8fbe388d6d6a1300505f0c
Jordan <61058266+J0RDYL33@users.noreply.github.com>
2023-01-17 11:37:50 +0000

    InitialCommit

---
+++ simulation.h
@@ -0,0 +1,100 @@
+/*-----------------------------------------------------------
+  Simulation Header File
+  -----------------------------------------------------------*/
+#include"vecmath.h"
+
+/*-----------------------------------------------------------
+  Macros
+  -----------------------------------------------------------*/
+#define TABLE_X			(0.6f) 
+#define TABLE_Z			(5.2f)
+#define TABLE_Y			(0.1f)
+#define BALL_RADIUS		(0.05f)
+#define BALL_MASS		(0.1f)
+#define TWO_PI			(6.2832f)
+#define	SIM_UPDATE_MS	(10)
+#define NUM_BALLS		(7)		
+#define NUM_CUSHIONS	(4)		
+
+/*-----------------------------------------------------------
+  plane normals
+  -----------------------------------------------------------*/
+/*
+extern vec2	gPlaneNormal_Left;
+extern vec2	gPlaneNormal_Top;
+extern vec2	gPlaneNormal_Right;
+extern vec2	gPlaneNormal_Bottom;
+*/
+
+
+/*-----------------------------------------------------------
+  cushion class
+  -----------------------------------------------------------*/
+class cushion
+{
+public:
+	vec2	vertices[2]; //2d
+	vec2	centre;
+	vec2	normal;
+
+	void MakeNormal(void);
+	void MakeCentre(void);
+};
+
+class fakeCushion : public cushion
+{
+
+};
+
+/*-----------------------------------------------------------
+  ball class
+  -----------------------------------------------------------*/
+
+class ball
+{
+	static int ballIndexCnt;
+public:
+	vec2	position;
+	vec2	velocity;
+	float	radius;
+	float	mass;
+	int		index;
+
+	ball(): position(0.0), velocity(0.0), radius(BALL_RADIUS), 
+		mass(BALL_MASS) {index = ballIndexCnt++; Reset();}
+	
+	void Reset(void);
+	void ApplyImpulse(vec2 imp);
+	void ApplyFrictionForce(int ms);
+	void DoPlaneCollision(const cushion &c);
+	void DoBallCollision(ball &b);
+	void Update(int ms);
+	
+	bool HasHitPlane(const cushion &c) const;
+	bool HasHitBall(const ball &b) const;
+
+	void HitPlane(const cushion &c);
+	void HitBall(ball &b);
+};
+
+/*-----------------------------------------------------------
+  table class
+  -----------------------------------------------------------*/
+class table
+{
+public:
+	ball balls[NUM_BALLS];	
+	cushion cushions[NUM_CUSHIONS];
+	fakeCushion fakeCushions[NUM_CUSHIONS];
+	void SetupCushions(void);
+	void SetCushionPosition(int cushionIndex, float vertices00, float vertices01, float vertices10, float vertices11);
+	void SetupFakeCushions(void);
+	void SetFakeCushionPosition(int cushionIndex, float vertices00, float vertices01, float vertices10, float vertices11);
+	void Update(int ms);	
+	bool AnyBallsMoving(void) const;
+};
+
+/*-----------------------------------------------------------
+  global table
+  -----------------------------------------------------------*/
+extern table gTable;
